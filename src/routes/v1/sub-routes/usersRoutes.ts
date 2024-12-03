6bccae5a8f96d0c0eb2ad8589d0b781d726bb3bb
nabilghazir <nabil.ghaziro27@gmail.com>
2024-12-03 13:59:04 +0700

    feat (user) : adding routes

--- src/routes/v1/sub-routes/usersRoutes.ts
+++ src/routes/v1/sub-routes/usersRoutes.ts
@@ -3,5 +3,10 @@ import userController from '../../../controllers/userController';
 
 const router = Router();
 
-router.get('/');
+router.get('/users', userController.getAllUsers.bind(userController));
+
+router.get('/users/:id', userController.getUserById.bind(userController));
+
+router.patch('/users/:id', userController.updateUser.bind(userController));
+
 export default router;
