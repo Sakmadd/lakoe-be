71d32d9ec65cf93b27dc5de1487646e17ebb7c0b
Dongayyy <165630423+nabilghazir@users.noreply.github.com>
2024-12-03 13:15:53 +0700

    feat(auth) : adding login features (#7)

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export const CONFIGS = {
   PORT: process.env.PORT,
   SALT_ROUND: process.env.SALT_ROUND,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
--- src/validator/dataSchema.ts
+++ src/validator/dataSchema.ts
@@ -5,3 +5,8 @@ export const registerSchema = z.object({
   email: z.string().email(),
   password: z.string().min(4).max(20),
 });
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(4).max(20),
+});
