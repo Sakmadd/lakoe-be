2cf617d690359acc3b6fb3358bafc8d3adc85a67
nabilghazir <nabil.ghaziro27@gmail.com>
2024-12-10 16:09:06 +0700

    fix(product_admin) : first commit

--- src/routes/v1/sub-routes/productsRoutes.ts
+++ src/routes/v1/sub-routes/productsRoutes.ts
@@ -1,7 +1,27 @@
 import { Router } from 'express';
+import productController from '../../../controllers/productController';
+import { authentication } from '../../../middlewares/authentication';
+import upload from '../../../middlewares/upload-file';
 
 const router = Router();
-router.get('/', (req, res) => {
-  res.send('Products routes');
-});
+
+router.get('/', productController.getAllProducts.bind(productController));
+
+router.post(
+  '/',
+  authentication,
+  upload.array('Images'),
+  productController.createProduct.bind(productController),
+);
+
+router.delete(
+  '/',
+  authentication,
+  productController.deleteProducts.bind(productController),
+);
+
+router.get('/search', productController.searchProducts.bind(productController));
+
+router.get('/:id', productController.getProductById.bind(productController));
+
 export default router;
