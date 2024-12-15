43a1c51953bf2d8e613c7ec7f2e5c398910c0514
farhanhans21 <farhanpra77@gmail.com>
2024-12-15 10:42:17 +0700

    'future(postOrder)'

---
+++ src/dtos/orders/createOrders.ts
@@ -0,0 +1,54 @@
+import { PaymentType } from '../../types/types';
+import { PaymentDTO } from '../admin/transactionDTO';
+
+export type CreateOrdersDTO = {
+  Recipient?: RecipientDTO;
+  total_price: number;
+  OrderItem: OrderItemType;
+  Payment: PaymentOrderDTO;
+};
+interface RecipientDTO {
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+  district: string;
+  city: string;
+  longitude: string;
+  latitude: string;
+}
+
+interface OrderItemType {
+  recipientId: string;
+  quantity: number;
+  Product?: Product;
+}
+
+export interface Product {
+  shop_id: string;
+  category_id: string;
+  name: string;
+  sku: string;
+  price: number;
+  url_name: string;
+  description: string;
+  stock: number;
+  weight: number;
+  minimum_order: number;
+  is_active: boolean;
+  length: number;
+  width: number;
+  height: number;
+}
+
+export interface PaymentOrderDTO {
+  invoice_id?: string;
+  mt_order_id?: string;
+  type: string;
+  url: string;
+  bank?: string;
+  amount: number;
+  account_name: string;
+  account_number: string;
+  status: string;
+}
