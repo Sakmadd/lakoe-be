836168caf93d435d8f670f028170b407f1f3980c
SAKMADD <ahmadsafii.work@gmail.com>
2024-12-28 14:44:19 +0700

    save

--- src/controllers/invoiceController.ts
+++ src/controllers/invoiceController.ts
@@ -107,8 +107,6 @@ class invoiceController {
     const { error, message, payload } =
       await invoiceService.getAllInvoiceBySellerId(id);
 
-    console.log(payload);
-
     if (error) {
       return res.status(404).json(
         new ResponseDTO({
